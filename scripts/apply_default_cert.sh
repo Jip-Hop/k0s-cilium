2733818208b477bfeac7c9f5bf9facd0de85d573
Jip-Hop <2871973+Jip-Hop@users.noreply.github.com>
2024-11-22 19:38:42 +0100

    Print to the stdout monitored by docker

--- scripts/apply_default_cert.sh
+++ scripts/apply_default_cert.sh
@@ -2,12 +2,14 @@
 set -euo pipefail
 umask 077
 
-echo "$(date) - about to crete new secret!" >/var/log/apply_default_cert.log
+(
+    echo 'About to crete new secret!'
 
-k0s kubectl create -n gateway-infra secret tls default-cert \
-    --key=/run/secrets/tls/tls.key \
-    --cert=/run/secrets/tls/tls.crt \
-    --dry-run=client \
-    -oyaml >/var/lib/k0s/manifests/tls/default-cert.yaml
+    k0s kubectl create -n gateway-infra secret tls default-cert \
+        --key=/run/secrets/tls/tls.key \
+        --cert=/run/secrets/tls/tls.crt \
+        --dry-run=client \
+        -oyaml >/var/lib/k0s/manifests/tls/default-cert.yaml
 
-echo "$(date) - done creating new secret!" >/var/log/apply_default_cert.log
+    echo 'Done creating new secret!'
+) &>/proc/1/fd/1
